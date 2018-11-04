70f8f39d30ee37c654c5e261b2e9fa4ad3137d11
Thomas Koudela <thomas@koudela.net>
2018-11-04 22:08:42 +0100

    add print event inheritance tree (e.g. for debug)

--- src/Event/RootEvent.php
+++ src/Event/RootEvent.php
@@ -19,4 +19,38 @@ class RootEvent extends Event
         $this->terminateSelf();
         $this->terminateOthers();
     }
+
+    public function toString(): string
+    {
+        $str = "root:\n";
+        $str .= $this->parameterToString($this);
+        $str .= "  children:\n";
+        $str .= $this->childrenToString($this, '    ');
+        return $str;
+    }
+
+    protected function parameterToString(Event $event, $indent = '  '): string
+    {
+        $treeIdentifier = $event->getTree() ? $event->getTree()->getIdentifier() : '';
+        return
+            $indent . 'tree: ' . $treeIdentifier . "\n" .
+            $indent . 'start: [' . implode(', ', $event->getStart()) . "]\n" .
+            $indent . 'destination: [' . implode(', ', $event->getDestination()) . "]\n" .
+            $indent . 'maxDepth: ' . $event->getMaxDepth() ?? 'null' . "\n";
+    }
+
+    protected function childrenToString(Event $parent, $indent = '  '): string
+    {
+        $str = '';
+
+        foreach ($parent->getChildren() as $name => $event)
+        {
+            $str .= $indent . $name . ":\n";
+            $str .= $this->parameterToString($event, $indent . '  ');
+            $str .= $indent . "  children:\n";
+            $str .= $this->childrenToString($event, $indent . '    ');
+        }
+
+        return $str;
+    }
 }
